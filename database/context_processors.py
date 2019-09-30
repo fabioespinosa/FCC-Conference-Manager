1121c7be2c0a773166a4a7c2366ba1dfb921f92b
Fabio <fa.espinosa10@uniandes.edu.co>
2019-09-30 08:29:12 +0200

    add user profile

--- database/context_processors.py
+++ database/context_processors.py
@@ -1,4 +1,5 @@
 # from django.contrib.auth.models import User
+from .models import UserProfile
 
 
 # We want the user messages to show in all views
@@ -6,7 +7,7 @@ def user_messages(request):
     user = request.user
     message_list = {'message_list': []}
     if user.is_authenticated:
-        userprofile = user.userprofile
+        userprofile = UserProfile.objects.get_or_create(user=user)
         message_list['message_list'] = userprofile.messages_received.filter(
             seen=False)
 
